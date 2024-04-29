03ed0f5fbbff589994370decb16dbd9cc667f25f
JoseCortezKinal <josecortezkinal@gmail.com>
2024-04-29 11:15:15 -0600

    initial commit

---
+++ src/org/carloscortez/system/Main.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.carloscortez.system;
+
+import java.io.InputStream;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import org.carloscortez.controller.FormClienteController;
+import org.carloscortez.controller.MenuClienteController;
+import org.carloscortez.controller.MenuPrincipalController;
+
+/**
+ *
+ * @author Carlos
+ */
+public class Main extends Application {
+    private final String URLVIEW = "/org/carloscortez/view/";
+    private Stage stage;
+    private Scene scene;
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.stage = stage;
+        stage.setTitle("Super Kinal APP");
+        menuPrincipalView();
+        stage.show();
+    }
+    
+    public Initializable switchScene(String fxmlName, int width, int height) throws Exception{
+        Initializable resultado = null;
+        FXMLLoader loader = new FXMLLoader();
+        
+        InputStream file = Main.class.getResourceAsStream(URLVIEW + fxmlName);
+        loader.setBuilderFactory(new JavaFXBuilderFactory());
+        loader.setLocation(Main.class.getResource(URLVIEW + fxmlName));
+        
+        scene = new Scene((AnchorPane) loader.load(file), width, height);
+        stage.setScene(scene);
+        stage.sizeToScene();
+        
+        resultado = (Initializable)loader.getController();
+                
+        return resultado;
+    }
+    
+    public void menuPrincipalView(){
+        try{
+            MenuPrincipalController menuPrincipalView = (MenuPrincipalController)switchScene("MenuPrincipalView.fxml", 950, 675);
+            menuPrincipalView.setStage(this);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void menuClienteView(){
+        try{
+            MenuClienteController menuClienteView = (MenuClienteController)switchScene("MenuClienteView.fxml", 1200, 750);
+            menuClienteView.setStage(this);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void formClienteView(int op){
+        try{
+            FormClienteController formClienteView = (FormClienteController)switchScene("FormClienteView.fxml", 500, 750);
+            formClienteView.setOp(op);
+            formClienteView.setStage(this);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
